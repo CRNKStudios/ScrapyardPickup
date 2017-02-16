9385f3a9edfc912954002f39bda4d380243b44a6
Spencer Pollock <srepollock@gmail.com>
2017-02-15 23:56:30 -0800

    :sleepy: Created a menu scene and MenuViewController.swift file. Going to look into better ways to manage the naviation, as well as Swift documentation before continuing.

---
+++ ScrapyardPickup/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  ScrapyardPickup
+//
+//  Created by Spencer Pollock on 2017-02-15.
+//  Copyright © 2017 CRNK Studios. All rights reserved.
+//
+
+import Foundation
+
+class MenuViewController {
+    
+}
